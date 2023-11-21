b5ec39f94a8f46b2f09efb0846a18f59dbc6d77a
anasbassi <anasabubakar2015@gmail.com>
2023-11-21 16:29:47 +0100

    Reactive forms conclution

---
+++ src/app/add-note/add-note.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-note',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-note.component.html',
+  styleUrl: './add-note.component.css'
+})
+export class AddNoteComponent {
+
+}
